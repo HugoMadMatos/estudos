640d3e8d788bc2d20d149a6e731ffc6d7c88f78a
Mylena <mylenastoduto@gmail.com>
2024-01-23 20:43:20 -0300

    adicionei novas linhas de código

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/bibi', (req, res) => {
+  const numero_1 = 1
+  const numero_2 = 2
+  res.send(`O valor é igual a: ${numero_1+numero_2}`)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
